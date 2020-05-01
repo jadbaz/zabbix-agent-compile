960af076a05a52123e0e0c3a078ceaf4435396aa
Jad Baz <jadbaz@gmail.com>
2020-05-01 15:27:15 +0300

    Update compile_zabbix_agent_openssl_static.sh

--- compile_zabbix_agent_openssl_static.sh
+++ compile_zabbix_agent_openssl_static.sh
@@ -22,7 +22,7 @@ ldconfig
 # https://unix.stackexchange.com/questions/293311/install-openssl-from-source
 cd /usr/local/src
 wget https://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz
-tar -xzvf openssl-$OPENSSL_VERSION.tar.gz
+tar -xzf openssl-$OPENSSL_VERSION.tar.gz
 cd openssl-$OPENSSL_VERSION
 
 time ./config --prefix=/usr/local/openssl --openssldir=/usr/local/openssl 
@@ -34,25 +34,32 @@ time make install
 cd /usr/local/src
 
 wget https://cdn.zabbix.com/stable/$ZABBIX_VERSION/zabbix-$ZABBIX_VERSION.tar.gz
-tar -xzvf zabbix-$ZABBIX_VERSION.tar.gz
+tar -xzf zabbix-$ZABBIX_VERSION.tar.gz
 cd zabbix-$ZABBIX_VERSION
 
 addgroup --system --quiet zabbix
 adduser --quiet --system --disabled-login --ingroup zabbix --home /var/lib/zabbix --no-create-home zabbix
 
-time ./configure --enable-agent --with-openssl=/usr/local/openssl --enable-static-libs
+time ./configure --enable-agent --with-openssl=/usr/local/openssl --enable-static-libs --enable-static
 time make
 
-ZABBIX_BIN_DIR=zabbix-$ZABBIX_VERSION-`uname -s`_`uname -m`
-mkdir $ZABBIX_BIN_DIR
-cp -t $ZABBIX_BIN_DIR src/zabbix_agent/zabbix_agentd src/zabbix_sender/zabbix_sender src/zabbix_get/zabbix_get
 
-tar -czvf ${ZABBIX_BIN_DIR}.tar.gz $ZABBIX_BIN_DIR
-md5sum ${ZABBIX_BIN_DIR}.tar.gz | awk '{print $1}' > ${ZABBIX_BIN_DIR}.tar.gz.md5
-cp -t /tmp/ ${ZABBIX_BIN_DIR}.tar.gz ${ZABBIX_BIN_DIR}.tar.gz.md5
+### PACKAGE FOR USE ELSEWHERE ###
+# remove unused large directories and remove their inclusion in all Makefile
+rm -rf frontends database
+sed -r '/^[\s\t]*database/d' -i Makefile
+rm -rf src/libs
+sed -r '/^[\s\t]*\$\(COMMON_SUBDIRS\)/d' -i src/Makefile
+
+
+cd ..
+ZABBIX_PACKAGE="zabbix-${ZABBIX_VERSION}_agent_dist_`uname -s`_`uname -m`.tar.gz"
+tar -czf ${ZABBIX_PACKAGE} zabbix-${ZABBIX_VERSION}
+md5sum ${ZABBIX_PACKAGE} | awk '{print $1}' > ${ZABBIX_PACKAGE}.md5
+cp -t /tmp/ ${ZABBIX_PACKAGE} ${ZABBIX_PACKAGE}.md5
 
 echo
 echo "**********************************************************************"
-echo "Compiled zabbix agent binaries at: /tmp/${ZABBIX_BIN_DIR}.tar.gz"
-echo "md5sum file: /tmp/${ZABBIX_BIN_DIR}.tar.gz.md5"
-echo "md5sum: `cat ${ZABBIX_BIN_DIR}.tar.gz.md5`"
+echo "Compiled zabbix agent dist: /tmp/${ZABBIX_PACKAGE}"
+echo "Compiled zabbix agent dist md5: /tmp/${ZABBIX_PACKAGE}.md5"
+echo "md5sum: `cat ${ZABBIX_PACKAGE}.md5`"
